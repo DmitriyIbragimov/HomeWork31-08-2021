6fe5a0ea141401e3a6f9f18a7aa895822d0dcfb7
DmitriyIbragimov <89562219+DmitriyIbragimov@users.noreply.github.com>
2021-09-12 21:39:07 +0300

    update

--- Home Work 3/task 9.py
+++ Home Work 3/task 9.py
@@ -4,6 +4,6 @@ c = int(input('Введите третье число: '))
 if b < a < c or c < a < b:
     print('Среднее число:', a)
 elif a < b < c or c < b < a:
-    print('Среднее:', b)
+    print('Среднее число:', b)
 else:
-    print('Среднее:', c)
+    print('Среднее число:', c)
