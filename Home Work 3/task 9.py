06839c9074414072aa079c2d5242e342666e35bb
DmitriyIbragimov <89562219+DmitriyIbragimov@users.noreply.github.com>
2021-09-20 20:46:43 +0300

    fixed

--- Home Work 3/task 9.py
+++ Home Work 3/task 9.py
@@ -1,18 +1,18 @@
 a = int(input('Введите первое число: '))
 b = int(input('Введите второе число: '))
 c = int(input('Введите третье число: '))
-if a > c and a > b:
-    if c > b:
+if a >= c and a >= b:
+    if c >= b:
         print('Среднее число:', c)
     else:
         print('Среднее число:', b)
-elif b > a and  b > c:
-    if a > c:
+elif b >= a and  b >= c:
+    if a >= c:
         print('Среднее число:', a)
     else:
         print('Среднее число:', c)
-elif c > a and c > b:
-    if a > b:
+elif c >= a and c >= b:
+    if a >= b:
         print('Среднее число:', a)
     else:
         print('Среднее число:', b)
