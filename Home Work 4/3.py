1ea32042300bad89e578e2151204fb6b88557d46
DmitriyIbragimov <89562219+DmitriyIbragimov@users.noreply.github.com>
2021-12-03 11:12:34 +0300

    fixed

--- Home Work 4/3.py
+++ Home Work 4/3.py
@@ -1,11 +1,10 @@
-import string
-
 n = int(input('Введи число от 1 до 52 '))
+m = range(65,65+n)
 if n <=52:
-    s = string.ascii_uppercase + string.ascii_lowercase
-    for i in range(n):
-        if i == len(s):
-            break
-        print(s[i], end='')
+    for i in m:
+        if i > 90:
+            print(chr(i + 6), end = '')
+        else:
+            print(chr(i), end = '')
 else:
     print('Говорю же от 1 до 52, а ты ввёл',n)
