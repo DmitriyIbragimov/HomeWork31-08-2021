63db787b1bb540f03c7a5c0f2c8cf16c3aa4ef6c
DmitriyIbragimov <89562219+DmitriyIbragimov@users.noreply.github.com>
2022-02-03 18:20:14 +0300

    Delete Home work5 directory

--- Home work5/2.py
+++
@@ -1,6 +0,0 @@
-list = ['q', 'x', 'ы','R' , 'л', 'j', 'y', 'Q', ' ', ' ', '.', '.']
-stroka = ""
-for i in list:
-            if ord(i) in range(97, 122) or ord(i) in range(65, 90) or ord(i) == 46 or ord(i) == 32:
-                                                                                                    stroka += i
-print(stroka)
